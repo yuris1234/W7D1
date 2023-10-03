aa6c70a09446e9e29f7f2208d3e3250fd8079071
yuris <yls4@cornell.edu>
2023-10-03 11:55:45 -0400

    added login! and logout! methods to applicationcontroller

--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/application_controller.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/application_controller.rb
@@ -3,4 +3,13 @@ class ApplicationController < ActionController::Base
     def current_user
         @current_user ||= User.find_by(session_token: session[:session_token])
     end
+
+    def login!(user)
+        @current_user = user 
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def logout!
+
+    end
 end
--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/users_controller.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
     def create
         user = User.new(user_params)
         if user.save
+            user.login!
             redirect_to cats_url
         else
             render :new
